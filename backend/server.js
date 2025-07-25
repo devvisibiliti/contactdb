c95fce42458eeea8caf8a221dbb2500989404041
devvisibiliti <dev@visibiliti.in>
2025-07-25 14:57:48 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api/appointments', appointmentRoutes);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 //Redirect all unmatched routes to index.html (for react Router)
-app.get('*', (req, res)=>{
+app.all('/{*any}', (req, res)=>{
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
